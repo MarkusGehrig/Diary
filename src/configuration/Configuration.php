594b7596f3b923dd99eb550397ea58042ab9f942
Markus Gehrig <markus.gehrig96@gmail.com>
2018-11-23 18:38:59 +0100

    working on configuration.php

--- src/configuration/Configuration.php
+++ src/configuration/Configuration.php
@@ -5,9 +5,21 @@
 return [
     // Database Configuration
     'database' => [
+
+        // Server URL, IP, DNS-Name
         'server'   => 'db',
+
+        // Database user
         'user'     => 'root',
+
+        // Database password
         'password' => 'geheim',
+
+        // Database name
         'database' => 'dairy',
+
+        // Driver (which type of database is used)
+        // Allowed values are pdo_mysql, ibm_db2, pdo_sqlsrv, pdo_pgsql, pdo_sqlite
+        'driver' => 'pdo_mysql',
     ],
 ];
