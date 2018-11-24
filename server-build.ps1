cd1062426796f71f72144a6f26c70444d948ff75
Markus Gehrig <markus.gehrig96@gmail.com>
2018-11-24 08:35:30 +0100

    working on server-build.ps1

--- server-build.ps1
+++ server-build.ps1
@@ -1,8 +1,8 @@
 docker network create dairy --driver bridge
 
 # Start a docker container for php and apache
-docker run --rm --network dairy -dit -p 80:80 -v C:\Projects\Dairy\src:/var/www/html php:7.2-apache 
+docker run --rm --network dairy -dit -p 80:80 -v $PSScriptRoot\src:/var/www/html php:7.2-apache 
 
 # Start a docker mysql container
-docker run --rm --network dairy --name db -p 3306:3306 -v C:\Projects\Dairy\mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=geheim -dit mysql:8 
+docker run --rm --network dairy --name db -p 3306:3306 -v $PSScriptRoot\mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=geheim -dit mysql:8 
 
