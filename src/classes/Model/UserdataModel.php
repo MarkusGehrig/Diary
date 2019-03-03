5ff5b2636c35425c02f6ef9754d79bad9acedc99
Markus Gehrig <markus.gehrig96@gmail.com>
2019-03-03 17:42:28 +0100

    fix format bug at insert statement for userdata

--- src/classes/Model/UserdataModel.php
+++ src/classes/Model/UserdataModel.php
@@ -211,7 +211,7 @@ class UserdataModel
                         'password' =>   $queryBuilder->createNamedParameter($this->getPassword()),
                         'surname' =>    $queryBuilder->createNamedParameter($this->getSurname()),
                         'lastname' =>   $queryBuilder->createNamedParameter($this->getLastname()),
-                        'active' =>     $queryBuilder->createNamedParameter($this->getActive())
+                        'active' =>     $queryBuilder->createNamedParameter($this->getActive(), PDO::PARAM_INT)
                     ]
                 )
                 ->where($queryBuilder->createNamedParameter('id = ' . $this->getId()))
